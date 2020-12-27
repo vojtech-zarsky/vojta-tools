1ef5f5052fc1efe28a11a1c8e3d6879880864dc7
Vojtech Zarsky <68601084+vojtech-zarsky@users.noreply.github.com>
2020-12-26 22:47:03 -0800

    Add files via upload

--- SeqIO.py
+++ SeqIO.py
@@ -14,10 +14,24 @@ class Seq:
 
 class SeqIO:
     def __init__(self, file):
-        self.file = file
+        if hasattr(file, 'read'):
+            self.name = file.name
+            self.file = file
+            self.byte = False
+        elif file[-3:] == '.gz':
+            self.name = file
+            import indexed_gzip
+            self.file = indexed_gzip.IndexedGzipFile(self.name)
+            self.byte = True
+        else:
+            self.name = file
+            self.file = open(self.name)
+            self.byte = False
         self.end = 1
         sLast = ''
         for sLine in self.file:
+            if self.byte:
+                sLine = sLine.decode()
             sLine = sLine.strip('\n')
             if re.search('^>', sLast) and len(sLine) > 0 and sLine[0] != '\s':
                 self.header = sLast.lstrip('>').strip('\n')
@@ -37,7 +51,10 @@ class SeqIO:
         sLine = ''
         while True:
             try:
-                sLine = next(self.file).strip('\n')
+                sLine = next(self.file)
+                if self.byte:
+                    sLine = sLine.decode()
+                sLine = sLine.strip('\n')
                 if re.search('^>',sLine):
                     seq = Seq(self.header.split()[0], self.header, self.seq)
                     self.header = sLine.lstrip('>').strip('\n')
@@ -59,26 +76,36 @@ class SeqIO:
             dSeqs[seq.id] = seq
         return dSeqs
     def writeIndex(self, sDb=None):
-        handle = open(self.file.name)
-        fileOut = open(handle.name+'.index','w')
+        if self.byte:
+            import indexed_gzip
+            handle = indexed_gzip.IndexedGzipFile(self.name)
+        else:
+            handle = open(self.name)
+        fileOut = open(self.name+'.index','w')
         iIndex = handle.tell()
         sLine = handle.readline()
+        if self.byte:
+            sLine = sLine.decode()
         if sDb == 'uniprot':
             while sLine:
                 if len(sLine) > 1 and sLine[0] == '>':
                     fileOut.write(sLine.split()[0].split('|')[2]+'\t'+str(iIndex)+'\n')
                 iIndex = handle.tell()
                 sLine = handle.readline()
+                if self.byte:
+                    sLine = sLine.decode()
         else:
             while sLine:
                 if len(sLine) > 1 and sLine[0] == '>':
                     fileOut.write(sLine.split()[0][1:]+'\t'+str(iIndex)+'\n')
                 iIndex = handle.tell()
                 sLine = handle.readline()
+                if self.byte:
+                    sLine = sLine.decode()
         return 0
     def readIndex(self):
         self.dId2Index = {}
-        for sLine in open(self.file.name+'.index'):
+        for sLine in open(self.name+'.index'):
             (sId, sIndex) = sLine.strip().split('\t',1)
             self.dId2Index[sId] = int(sIndex)
         return 0
@@ -86,9 +113,14 @@ class SeqIO:
         if iIndex == None:
             iIndex = self.dId2Index[sId]
         self.file.seek( iIndex )
-        sHeader = self.file.readline()[1:].strip()
+        sLine = self.file.readline()
+        if self.byte:
+            sLine = sLine.decode()
+        sHeader = sLine[1:].strip()
         sSeq = ''
         for sLine in self.file:
+            if self.byte:
+                sLine = sLine.decode()
             sLine = sLine.strip()
             if sLine == '' or sLine[0] == '>':
                 break
@@ -96,10 +128,18 @@ class SeqIO:
         return Seq(sHeader.split()[0], sHeader, sSeq)
 
 """
-seqio = SeqIO('test.fasta')
+import indexed_gzip
+#f = indexed_gzip.IndexedGzipFile('../databases/uniprot/uniprot_sprot.201002.fasta.gz')
+#print(dir(f))
+#print(f.__str__)
+seqio = SeqIO('temp.fasta')
+#seqio = SeqIO(open('temp.fasta'))
+for seq in seqio:
+    print(seq.getString())
+    break
 seqio.writeIndex()
 seqio.readIndex()
-seq = seqio.getSeq('XP_012898703.1')
-print seq.id, seq.seq
-"""
+seq = seqio.getSeq('sp|Q6GZX3|002L_FRG3G')
+print(seq.getString())
+#"""
 
